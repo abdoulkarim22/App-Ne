862ed2fccd805c6b72125110e4a150d148ea8e55
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-04-03 04:22:26 +0100

    add Text and  install the package

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import Header from './components/Header';
-import HeadLine from './components/HeadLine';
 import Page from './components/Page';
 
 export default function App() {
